5b2bccfbea5f09b156f77f95e8cc6e321020443d
SalvadorLekan <lekansalvador@gmail.com>
2020-11-30 19:31:30 +0100

    ..

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Auth({ formlist, formData, children }) {
   return (
-    <div className="bg-green-200 p-12 w-screen fixed overflow-scroll h-screen">
+    <div className="bg-green-200 p-9 w-screen fixed overflow-scroll h-screen">
       <form
         onSubmit={(e) => e.preventDefault()}
-        className="max-w-xl flex mx-auto overflow-visible flex-col bg-white p-6 m-1 rounded shadow"
+        className="max-w-xl flex mx-auto overflow-visible flex-col bg-white p-2 m-1 rounded shadow"
       >
         {children}
       </form>
