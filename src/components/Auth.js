615ae041935d5090687ccee8e25c7ef010d03f23
SalvadorLekan <lekansalvador@gmail.com>
2020-11-30 19:26:53 +0100

    /

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function Auth({formlist, formData, children}) {
-    return (
-        <div className="bg-green-200 p-16 w-screen fixed overflow-scroll h-screen">
-            <form onSubmit={e=>e.preventDefault()} className="max-w-xl flex mx-auto overflow-visible flex-col bg-white p-12 m-4 rounded shadow">
-                {children}
-            </form>
-        </div>
-    )
+function Auth({ formlist, formData, children }) {
+  return (
+    <div className="bg-green-200 p-16 w-screen fixed overflow-scroll h-screen">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="max-w-xl flex mx-auto overflow-visible flex-col bg-white p-12 m-1 rounded shadow"
+      >
+        {children}
+      </form>
+    </div>
+  );
 }
 
-export default Auth
+export default Auth;
