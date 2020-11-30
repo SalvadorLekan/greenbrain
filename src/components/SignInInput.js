814a2c713ca15cd759ec2d2f8eac87383ffcc05f
SalvadorLekan <lekansalvador@gmail.com>
2020-11-30 11:15:20 +0100

    Added Redirect rule

--- src/components/SignInInput.js
+++ src/components/SignInInput.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { FiMail } from 'react-icons/fi'
+import { FiMail, FiKey } from 'react-icons/fi'
 
 function SignInInput() {
     return (
+        <>
         <label className="flex items-center justify-between text-green-300 border-2 p-2 m-2 border-green-200">
             <div className="border-r-2 border-green-200 mr-2 pr-2"><FiMail/></div>
             <input placeholder="place"/>
         </label>
+        <label className="flex items-center justify-between text-green-300 border-2 p-2 m-2 border-green-200">
+            <div className="border-r-2 border-green-200 mr-2 pr-2"><FiKey/></div>
+            <input placeholder="place"/>
+        </label>
+        </>
     )
 }
 
